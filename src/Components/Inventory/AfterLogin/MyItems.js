c9fa76d181a8fa79caf677c20d73c43c6c12db8f
HackBomm <hackbomm.net@gmail.com>
2022-05-07 12:20:46 +0600

    Update myitem

--- src/Components/Inventory/AfterLogin/MyItems.js
+++ src/Components/Inventory/AfterLogin/MyItems.js
@@ -3,7 +3,11 @@ import useProducts from "./../../Shared/useProducts";
 
 const MyItems = () => {
   const [products, setProducts] = useProducts();
-  return <div>{}</div>;
+  return (
+    <div>
+      <h2>Updating My Item</h2>
+    </div>
+  );
 };
 
 export default MyItems;
